11e1c745f0c7f7eeb67b2710a03d46fd630e089f
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-13 16:24:53 +0900

    Update

--- experiments/main_EDSR_Experiment.py
+++ experiments/main_EDSR_Experiment.py
@@ -1,5 +1,4 @@
-# import trainer_v1_pool as trainer
-import trainer_v2_centered_init as trainer
+
 from models.EDSR import EDSR
 from utils.data_loader import get_loader
 import torch
@@ -11,5 +10,22 @@ model = EDSR()
 train_loader = get_loader(mode='train', batch_size=16, augment=True)
 test_loader = get_loader(mode='test')
 
+# import trainer_v1_pool as trainer
 # trainer.train(model, train_loader, test_loader, mode='EDSR_v1_pool')
-trainer.train(model, train_loader, test_loader, mode='EDSR_v2_centered_kernel')
+
+# import trainer_v2_centered_init as trainer
+# trainer.train(model, train_loader, test_loader, mode='EDSR_v2_centered_kernel')
+
+
+# import trainer_v3_grad_init as trainer
+# trainer.train(model, train_loader, test_loader, mode='EDSR_v3_grad_kernel')
+
+
+# import trainer as trainer
+# from models.EDSR_feature_mlp import EDSR
+# model = EDSR()
+# trainer.train(model, train_loader, test_loader, mode='EDSR_v4_feature_mlp')
+
+
+import trainer_v4_fft as trainer
+trainer.train(model, train_loader, test_loader, mode='EDSR_v4_fft')
