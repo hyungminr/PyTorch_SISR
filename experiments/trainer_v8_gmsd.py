1b2420fd2f0e6217f1cf0a49d7e5442c0c7ddf36
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-19 15:22:54 +0900

    Update

--- experiments/trainer_v8_gmsd.py
+++ experiments/trainer_v8_gmsd.py
@@ -83,7 +83,7 @@ def train(model, train_loader, test_loader, mode='EDSR_Baseline', save_image_eve
     for key in ['epoch', 'psnr', 'ssim', 'ms-ssim']:
         hist[key] = []
 
-    pretrained_epoch = 500
+    pretrained_epoch = 200
 
     for epoch in range(num_epochs):
         if epoch < pretrained_epoch: continue
