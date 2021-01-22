9d1079c0acf576a00e0b80808a29bdaaa639db07
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-22 14:24:28 +0900

    Update

--- experiments/main_EDSR_Experiment.py
+++ experiments/main_EDSR_Experiment.py
@@ -166,10 +166,10 @@ elif scale_factor == 2:
 # import trainer_v20_gmsd_masked_loss as trainer
 # trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v20_gmsd_masked_loss')
 
-"""
 import trainer_v21_high_freq as trainer
 
 from models.EDSR_high_freq import EDSR
+scale_factor = 4
 model = EDSR(scale=scale_factor)
 
 if scale_factor == 4:
@@ -180,6 +180,7 @@ elif scale_factor == 2:
     test_loader = get_loader(mode='test')
 
 trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v21_high_freq')
+
 """
 
 from models.EDSR_x1 import EDSR
@@ -187,3 +188,4 @@ model = EDSR(scale=scale_factor)
 import trainer_deblurer as trainer
 trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_deblur', epoch_start=0, num_epochs=1000)
 
+"""
