f759857e6d9ce6acc495bf200d8dc49f615eabaf
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-22 19:35:10 +0900

    Update

--- experiments/main_EDSR_Experiment.py
+++ experiments/main_EDSR_Experiment.py
@@ -199,4 +199,7 @@ pretrained = torch.load('./weights/2021.01.22/EDSR_x2_deblur/epoch_1000.pth')
 for n, p in model.named_parameters():
     if 'tail' not in n: p.data.copy_(pretrained[n])    
 import trainer
-trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v23_sr_with_pretrained_deblur')
+# trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v23_sr_with_pretrained_deblur')
+for n, p in model.named_parameters():
+    if 'tail' not in n: p.requires_grad = False
+trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v24_sr_with_pretrained_deblur_fixed')
