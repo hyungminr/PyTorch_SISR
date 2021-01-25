f7b47c69a66d90d41f73477c4c3027ca6a9ee962
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-25 14:21:21 +0900

    Update

--- experiments/main_EDSR_Experiment (copy).py
+++ experiments/main_EDSR_Experiment (copy).py
@@ -221,13 +221,7 @@ trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v21_
 
 import trainer_v27_sliding_gramm_loss as trainer
 trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v27_sliding_gramm_loss')
-
+"""
 
 import trainer_v28_sliding_gramm_loss_on_img as trainer
 trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v28_sliding_gramm_loss_on_img')
-"""
-
-from models.EDSR_intermediate_results import EDSR
-model = EDSR(scale=scale_factor)
-import trainer_v29_inter_results as trainer
-trainer.train(model, train_loader, test_loader, mode=f'EDSR_x{scale_factor}_v29_inter_results')
