b8b733b65b62b27727c21ba101dce8f22773260b
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-28 17:04:51 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -12,7 +12,7 @@ torch.manual_seed(0)
 scale_factor = 4
 model = hmnet(scale=scale_factor)
 if scale_factor == 4:
-    train_loader = get_loader(mode='train', batch_size=16, height=192, width=192, scale_factor=4, augment=True)
+    train_loader = get_loader(mode='train', batch_size=8, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v1_hf', epoch_start=0, num_epochs=1000, save_model_every=100)
 
