ef47337c49ff7203e0e0d8907e0e5e4f5794a679
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-28 17:10:42 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -38,7 +38,18 @@ model = hmnet(scale=scale_factor)
 if scale_factor == 4:
     train_loader = get_loader(mode='train', batch_size=8, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)    
-trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Baseline_batch_8', epoch_start=0, num_epochs=5000, save_model_every=100)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Baseline_batch_8', epoch_start=0, num_epochs=3000, save_model_every=100)
+
+from models.hmnet import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+model = hmnet(scale=scale_factor)
+if scale_factor == 4:
+    train_loader = get_loader(mode='train', batch_size=1, height=192, width=192, scale_factor=4, augment=True)
+    test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)    
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Baseline_batch_1', epoch_start=0, num_epochs=3000, save_model_every=100)
 
 
 
