c80aba72cdd0dc4ccaf649681972dc5b443a3435
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-28 16:44:22 +0900

    Update

--- bin2freq.py
+++ bin2freq.py
@@ -17,17 +17,18 @@ t = T.Compose(transform)
 data_dir = []
 data_dir.append('./data/DIV2K/bin/DIV2K_train_HR/*pt')
 data_dir.append('./data/DIV2K/bin/DIV2K_valid_HR/*pt')
-#data_dir.append('./data/DIV2K/bin/DIV2K_train_LR_bicubic/X2/*pt')
-#data_dir.append('./data/DIV2K/bin/DIV2K_valid_LR_bicubic/X2/*pt')
-#data_dir.append('./data/DIV2K/bin/DIV2K_train_LR_bicubic/X4/*pt')
-#data_dir.append('./data/DIV2K/bin/DIV2K_valid_LR_bicubic/X4/*pt')
+data_dir.append('./data/DIV2K/bin/DIV2K_train_LR_bicubic/X2/*pt')
+data_dir.append('./data/DIV2K/bin/DIV2K_valid_LR_bicubic/X2/*pt')
+data_dir.append('./data/DIV2K/bin/DIV2K_train_LR_bicubic/X4/*pt')
+data_dir.append('./data/DIV2K/bin/DIV2K_valid_LR_bicubic/X4/*pt')
 
 for d in data_dir:
     images = glob.glob(d)
     for iname in tqdm(images):
+        rname = iname.replace('/DIV2K/bin/', '/DIV2K/bin/hfreq/')
+        if os.path.exists(rname): continue
         tensor = torch.load(iname)
         tensor = high_pass_filter_hard_kernel(tensor.unsqueeze(0))
         tensor = tensor.squeeze(0)
-        rname = iname.replace('/DIV2K/bin/', '/DIV2K/bin/hfreq/')
         os.makedirs('/'.join(rname.split('/')[:-1]), exist_ok=True)
         torch.save(tensor, rname)
