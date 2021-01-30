68a558bb5919e8aa8019d68675fcaef5cd1508da
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-30 09:41:28 +0900

    Update

--- bin2freq_REDS.py
+++ bin2freq_REDS.py
@@ -21,13 +21,18 @@ data_dir.append('./data/benchmark/REDS/bin/val/val_sharp/*/*.pt')
 data_dir.append('./data/benchmark/REDS/bin/val/val_blur_bicubic/X4/*/*.pt')
 data_dir.append('./data/benchmark/REDS/bin/test/test_blur_bicubic/X4/*/*.pt')
 
+failure_cases = []
 for d in data_dir:
     images = glob.glob(d)
     for iname in tqdm(images):
         rname = iname.replace('/bin/', '/bin/hfreq/')
         if os.path.exists(rname): continue
-        tensor = torch.load(iname)
-        tensor = high_pass_filter_hard_kernel(tensor.unsqueeze(0))
-        tensor = tensor.squeeze(0)
-        os.makedirs('/'.join(rname.split('/')[:-1]), exist_ok=True)
-        torch.save(tensor, rname)
+        try:
+            tensor = torch.load(iname)
+            tensor = high_pass_filter_hard_kernel(tensor.unsqueeze(0))
+            tensor = tensor.squeeze(0)
+            os.makedirs('/'.join(rname.split('/')[:-1]), exist_ok=True)
+            torch.save(tensor, rname)
+        except:
+            failure_cases.append(iname)
+print(filure_cases)
