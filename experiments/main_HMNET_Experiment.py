51b634e192a859d42bc49e5810cd7fc6c2335f17
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-01 08:44:59 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -60,7 +60,7 @@ if scale_factor == 4:
     train_loader = get_loader(mode='train', batch_size=16, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)    
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy', epoch_start=0, num_epochs=3000, save_model_every=100)
-"""
+
 
 from models.hmnet_heavy import hmnet
 from utils.data_loader import get_loader
@@ -72,3 +72,15 @@ if scale_factor == 4:
     train_loader = get_loader(data='REDS', mode='train', batch_size=16, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)    
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS', epoch_start=0, num_epochs=3000, save_model_every=1, test_model_every=10)
+
+"""
+from models.hmnet_heavy import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+model = hmnet(scale=scale_factor)
+if scale_factor == 4:
+    train_loader = get_loader(mode='train', batch_size=1, height=192, width=192, scale_factor=4, augment=True)
+    test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)    
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_1', epoch_start=0, num_epochs=3000, save_model_every=100, test_model_every=1)
