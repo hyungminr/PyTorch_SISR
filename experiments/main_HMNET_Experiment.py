7c10cfb224355341d2fd439ae1c3a580aa99d5be
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-01-31 21:41:06 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -71,4 +71,4 @@ model = hmnet(scale=scale_factor)
 if scale_factor == 4:
     train_loader = get_loader(data='REDS', mode='train', batch_size=16, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)    
-trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS', epoch_start=0, num_epochs=3000, save_model_every=100, test_model_every=10)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS', epoch_start=0, num_epochs=3000, save_model_every=1, test_model_every=10)
