e060673c4849d61dd1707cdc0dc7ee25ebd90ca8
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-03 07:59:54 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -141,6 +141,7 @@ epoch_start = 300
 for _ in range(10):
     batch_size *= 2
     epoch_start += 200
+    if batch_size == 32: num_epochs = 2000
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_batch_{batch_size}', epoch_start=epoch_start, num_epochs=200, save_model_every=100, test_model_every=1)
@@ -207,7 +208,7 @@ for _ in range(10):
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1)
-    """
+    
     
 from models.hmnet_v3 import hmnet
 from utils.data_loader import get_loader
@@ -228,3 +229,32 @@ for _ in range(10):
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_hf_loss_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1)
+    
+  
+
+from models.high_pass_filter_lite_v2 import high_pass_filter
+from utils.data_loader_freq_high import get_loader
+import trainer_hmnet_v2_high_pass_filter as trainer
+torch.manual_seed(0)
+scale_factor = 4
+model = high_pass_filter(scale=scale_factor)
+if scale_factor == 4:
+    train_loader = get_loader(mode='train', batch_size=1, height=192, width=192, scale_factor=4, augment=True)
+    test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)    
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_High_Pass_Filter_lite_v2', epoch_start=0, num_epochs=3000, save_model_every=10, test_model_every=1)
+
+"""
+from models.hmnet_heavy import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+model = hmnet(scale=scale_factor)
+model.load_state_dict(torch.load('./weights/2021.02.02/HMNET_x4_Heavy_batch_16/epoch_1300.pth'))
+batch_size = 16
+epoch_start = 1300
+num_epochs = 2000
+train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1)
+
