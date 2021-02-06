612b609a63bb48ec69be66178664d7c29080c6a1
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-07 00:09:55 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -422,7 +422,7 @@ num_epochs = 10
 
 model = hmnet(scale=scale_factor)
 today = datetime.datetime.now().strftime('%Y.%m.%d')
-
+model.load_state_dict(torch.load('./weights/hmnet_heavy_3300.pth'))
 train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
 test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today)
