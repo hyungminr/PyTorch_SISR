907c18aab4ab6c24c66587c188282ee0980ac32c
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-08 07:53:46 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -435,4 +435,4 @@ while num_epochs <= 200:
     if batch_size == 32: num_epochs = 3000
     train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
-    trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today)
+    trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today, refresh=False)
