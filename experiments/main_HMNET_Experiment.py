1b147c9652cb5dc87e7b1ed01ba4e9c1e28e0b46
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-10 14:06:15 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -436,7 +436,7 @@ while num_epochs <= 200:
     train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today, refresh=False)
-"""
+
 
 from models.hmnet_v3 import hmnet
 from utils.data_loader import get_loader
@@ -463,3 +463,35 @@ while num_epochs <= 200:
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_denoiser', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
+    
+"""
+from models.hmnet_v3 import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 200
+
+model = hmnet(scale=scale_factor)
+model.load_state_dict(torch.load('./weights/hmnet_v3_SRGAN.pth'))
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+
+for n, p in model.named_parameters():
+    if 'tail' not in n:
+        p.requires_grad = False
+        
+train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_from_GAN_train_tail_only', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today)
+
+
+while num_epochs <= 200:
+    batch_size *= 2
+    epoch_start += 200
+    if batch_size == 32: num_epochs = 3000
+    train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+    test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+    trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_from_GAN_train_tail_only', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
