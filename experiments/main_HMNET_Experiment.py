a5a3992fd019249e69ebe1ce53985d0df53fd42c
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-10 15:40:01 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -536,14 +536,14 @@ num_epochs = 200
 model = hmnet(scale=scale_factor)
 today = datetime.datetime.now().strftime('%Y.%m.%d')
         
-train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
-test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=4)
+train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=1, augment=True)
+test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_REDS_JPEG', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=10, test_model_every=1, today=today)
 
 while num_epochs <= 200:
     batch_size *= 2
     epoch_start += 200
     if batch_size == 16: num_epochs = 3000
-    train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
-    test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=4)
+    train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=1, augment=True)
+    test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=1)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_REDS_JPEG', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=10, test_model_every=1, today=today, refresh=False)
