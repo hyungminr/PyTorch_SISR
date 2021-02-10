cba4933f12173c91c70054513423119d04b60e8b
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-10 14:47:58 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -464,7 +464,7 @@ while num_epochs <= 200:
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_denoiser', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
     
-"""
+
 from models.hmnet_v3 import hmnet
 from utils.data_loader import get_loader
 import trainer_hmnet as trainer
@@ -495,3 +495,26 @@ while num_epochs <= 200:
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_from_GAN_train_tail_only', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
+"""
+
+from models.hmnet_v3 import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 16
+epoch_start = 0
+num_epochs = 200
+
+model = hmnet(scale=scale_factor)
+model.load_state_dict(torch.load('./weights/hmnet_v3_SRGAN.pth'))
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+
+for n, p in model.named_parameters():
+    if 'tail' not in n:
+        p.requires_grad = False
+        
+train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_v3_from_GAN_train_tail_only', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
