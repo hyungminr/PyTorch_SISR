512034989b37fba621d065cf5f094e12ed108d48
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-15 16:52:24 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -567,7 +567,7 @@ model = hmnet(scale=scale_factor)
 today = datetime.datetime.now().strftime('%Y.%m.%d')
 today = '2021.02.16'
 model.load_state_dict(torch.load('./weights/2021.02.07/HMNET_x4_Heavy_REDS_batch_32/epoch_0166.pth'))
-"""
+
 train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
 test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today)
@@ -575,7 +575,7 @@ trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Hea
 while num_epochs <= 200:
     batch_size *= 2
     epoch_start += 10
-    if batch_size == 32: num_epochs = 3000
+    if batch_size == 16: num_epochs = 3000
     train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=2, test_model_every=1, today=today, refresh=False)
@@ -585,4 +585,5 @@ num_epochs = 3000
 train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
 test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_batch_{batch_size}', epoch_start=166, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+"""
 
