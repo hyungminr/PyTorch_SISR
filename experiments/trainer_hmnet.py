d79455b34fad05e5b20eca8549b802d3935932bd
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-18 11:01:07 +0900

    Update

--- experiments/trainer_hmnet.py
+++ experiments/trainer_hmnet.py
@@ -168,7 +168,9 @@ def train(model, train_loader, test_loader, mode='EDSR_Baseline', save_image_eve
                 else:
                     soft_mask = False
                 
-                
+                print(sr_.shape)
+                print(hr.shape)
+                print(lr.shape)
                 if soft_mask:
                     with torch.no_grad():
                         for _ in range(10): gmsd = opening(gmsd)
