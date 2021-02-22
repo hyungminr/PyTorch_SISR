8878ac01f4780f46bb9f003175b02246968618b5
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-22 09:57:54 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -631,7 +631,7 @@ num_epochs = 3000
 train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
 test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_size_{size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
-"""
+
 
 
 from models.hmnet_heavy_x1_ablation_edge import hmnet
@@ -649,7 +649,7 @@ today = datetime.datetime.now().strftime('%Y.%m.%d')
         
 train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
 test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
-trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_edge', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today)
 
 while num_epochs <= 200:
     batch_size *= 2
@@ -658,5 +658,31 @@ while num_epochs <= 200:
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_edge', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
+"""
+
+from models.hmnet_heavy_x1_ablation_fea import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet_denoiser_0210 as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 200
+
+model = hmnet(scale=scale_factor)
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+        
+train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today)
+
+while num_epochs <= 200:
+    batch_size *= 2
+    epoch_start += 200
+    if batch_size == 16: num_epochs = 3000
+    train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
+    test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
+    trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
     
 
