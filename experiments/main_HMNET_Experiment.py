bb23dbaf972609539359e11bf2d09f0e93b0a375
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-24 12:11:42 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -658,7 +658,7 @@ while num_epochs <= 200:
     train_loader = get_loader(mode='train', batch_size=batch_size, height=192, width=192, scale_factor=4, augment=True)
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_edge', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
-"""
+
 
 from models.hmnet_heavy_x1_ablation_fea import hmnet
 from utils.data_loader import get_loader
@@ -685,4 +685,28 @@ while num_epochs <= 200:
     test_loader = get_loader(mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_heavy_x1_denoise_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
     
+"""
+
+from models.hmnet_heavy import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0 - 10
+num_epochs = 100
+model = hmnet(scale=scale_factor)
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+today = '2021.02.24'
+model.load_state_dict(torch.load('./weights/2021.02.17/HMNET_x4_Heavy_REDS_size_352/epoch_0120.pth'))
+
+size = 192 - 192
+while num_epochs <= 200:
+    epoch_start += num_epochs
+    size += 192
+    if size > 512: num_epochs = 3000
+    train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+    test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
+    trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_size_{size}', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=2, today=today, refresh=False)
 
