4e2646f856d4fe2a8da4ae87f152e5b60aadd7a6
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-02-25 11:45:27 +0900

    Update

--- experiments/trainer_hmnet_hf_loss.py
+++ experiments/trainer_hmnet_hf_loss.py
@@ -173,8 +173,8 @@ def train(model, train_loader, test_loader, mode='EDSR_Baseline', save_image_eve
                 
                 
                 if soft_mask:
-                    with torch.no_grad():
-                        for _ in range(10): gmsd = opening(gmsd)
+                    # with torch.no_grad():
+                    #     for _ in range(10): gmsd = opening(gmsd)
                     gmask = gmsd / gmsd.max()
                     gmask = (gmask > 0.2) * 1.0
                     gmask = blur(gmask)                
