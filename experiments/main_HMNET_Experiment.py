e18e4c1ee1d00e052103ea9cbf9d3dbea2035044
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-03-02 10:36:47 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -772,7 +772,7 @@ train_loader = get_loader(data='SIDD', mode='train', batch_size=batch_size, heig
 test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_SIDD', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
 
-"""
+
 
 from models.hmnet_heavy import hmnet
 from utils.data_loader import get_loader
@@ -793,4 +793,25 @@ num_epochs = 3000
 train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
 test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_size_{size}', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+"""
 
+from models.hmnet_heavy_x1 import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet_REDS_jpeg as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 3000
+
+model = hmnet(scale=scale_factor)
+model.load_state_dict(torch.load('./weights/2021.02.26/HMNET_REDS_JPEG/epoch_0021.pth'))
+
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+
+size = 0
+num_epochs = 3000
+train_loader = get_loader(data='REDS_JPEG', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+test_loader = get_loader(data='REDS_JPEG', mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_REDS_JPEG_size_0', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
