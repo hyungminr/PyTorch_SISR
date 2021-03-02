84c0f3cb46908a61065df1cf383538d33deed077
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-03-02 10:32:15 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -752,8 +752,6 @@ train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size,
 test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_REDS_JPEG', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
 
-"""
-
 
 from models.hmnet_heavy_x1 import hmnet
 from utils.data_loader import get_loader
@@ -773,3 +771,26 @@ today = datetime.datetime.now().strftime('%Y.%m.%d')
 train_loader = get_loader(data='SIDD', mode='train', batch_size=batch_size, height=256, width=256, scale_factor=1, augment=True)
 test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_SIDD', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
+
+"""
+
+from models.hmnet_heavy import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet_hf_loss as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 10
+model = hmnet(scale=scale_factor)
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+today = '2021.03.02'
+model.load_state_dict(torch.load('./weights/2021.02.18/HMNET_x4_Heavy_REDS_size_0_hf_loss/epoch_0038.pth'))
+
+size = 0
+num_epochs = 3000
+train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_REDS_size_{size}', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+
