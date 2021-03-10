6564ea0c5954504be298eec4cd102252d63dae03
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-03-10 14:15:19 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -914,7 +914,7 @@ while num_epochs <= 200:
     test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_factor=4)
     trainer.train(model, train_loader, test_loader, mode=f'HMNET_SIDD_heavy_x1_denoise_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=100, test_model_every=1, today=today, refresh=False)
     
-"""
+
 
 today='2021.03.10'
 
@@ -939,6 +939,7 @@ test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_SIDD_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
     
 
+
 from models.hmnet_heavy_x_ab_edge_0310 import hmnet
 from utils.data_loader import get_loader
 import trainer_hmnet_REDS_jpeg as trainer
@@ -958,3 +959,27 @@ train_loader = get_loader(data='SIDD', mode='train', batch_size=batch_size, heig
 test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_SIDD_ablation_edge', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
     
+    
+    """
+
+
+
+from models.hmnet_heavy_ablation_fea import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet_hf_loss as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 10
+model = hmnet(scale=scale_factor)
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+model.load_state_dict(torch.load('./weights/2021.03.02/HMNET_x4_Heavy_no_fea_REDS_size_0/epoch_0027.pth'))
+
+size = 0
+num_epochs = 3000
+train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_Heavy_no_fea_REDS_size_{size}', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+
