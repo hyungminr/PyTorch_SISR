e1677f1f7d6a8a6284e93fb517d76b45be10889c
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-03-16 15:18:41 +0900

    Update

--- experiments/main_HMNET_Experiment.py
+++ experiments/main_HMNET_Experiment.py
@@ -1009,7 +1009,7 @@ model.load_state_dict(torch.load('./weights/HMNET_x4_Heavy_REDS_JPEG.pth'))
 train_loader = get_loader(data='SIDD', mode='train', batch_size=batch_size, height=256, width=256, scale_factor=1, augment=True)
 test_loader = get_loader(data='SIDD', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_x{scale_factor}_SIDD_ablation_fea', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
-"""
+
 
 from models.hmnet_heavy_x1_ablation_fea import hmnet
 from utils.data_loader import get_loader
@@ -1031,4 +1031,26 @@ num_epochs = 3000
 train_loader = get_loader(data='REDS_jpeg', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=14, augment=True)
 test_loader = get_loader(data='REDS_jpeg', mode='test', height=256, width=256, scale_factor=1)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_REDS_JPEG_last', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+
+
+"""
+from models.hmnet_heavy_ablation_fea import hmnet
+from utils.data_loader import get_loader
+import trainer_hmnet_hf_loss_last as trainer
+torch.manual_seed(0)
+scale_factor = 4
+
+batch_size = 1
+epoch_start = 0
+num_epochs = 10
+model = hmnet(scale=scale_factor)
+today = datetime.datetime.now().strftime('%Y.%m.%d')
+#model.load_state_dict(torch.load('./weights/2021.03.02/HMNET_x4_Heavy_no_fea_REDS_size_0/epoch_0027.pth'))
+
+size = 0
+num_epochs = 3000
+train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
+trainer.train(model, train_loader, test_loader, mode=f'HMNET_REDS_SR_last', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
+
 
