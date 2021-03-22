ec8502b7326d26daa337eafe5086b7666916e745
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-03-22 09:43:13 +0900

    Update

--- experiments/main_flickr2K.py
+++ experiments/main_flickr2K.py
@@ -9,6 +9,7 @@ import datetime
 today='2021.03.20'
 
 
+"""
 from models.hmnet_heavy_x1 import hmnet
 from utils.data_loader import get_loader
 import trainer_hmnet_Flickr2K as trainer
@@ -18,7 +19,6 @@ scale_factor = 4
 batch_size = 1
 epoch_start = 0
 num_epochs = 200
-
 model = hmnet(scale=scale_factor)
 #model.load_state_dict(torch.load('./weights/HMNET_x4_Heavy_REDS_JPEG.pth'))
 
@@ -26,6 +26,7 @@ train_loader = get_loader(data='Flickr2K', mode='train', batch_size=batch_size, 
 test_loader = get_loader(data='Flickr2K', mode='test', batch_size=batch_size, height=0, width=0, scale_factor=1, augment=True, force_size=True)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_Flickr2K', epoch_start=epoch_start, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today)
     
+"""
 from models.hmnet_heavy_x1_ab_fea_0310 import hmnet
 from utils.data_loader import get_loader
 import trainer_hmnet_Flickr2K as trainer
@@ -34,8 +35,7 @@ scale_factor = 4
 
 batch_size = 1
 epoch_start = 0
-num_epochs = 200
-
+num_epochs = 40
 model = hmnet(scale=scale_factor)
 #model.load_state_dict(torch.load('./weights/HMNET_x4_Heavy_REDS_JPEG.pth'))
 
@@ -52,8 +52,8 @@ scale_factor = 4
 
 batch_size = 1
 epoch_start = 0
-num_epochs = 200
-
+num_epochs = 40
+del model
 model = hmnet(scale=scale_factor)
 #model.load_state_dict(torch.load('./weights/HMNET_x4_Heavy_REDS_JPEG.pth'))
 
