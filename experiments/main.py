0cc340e538c52749efaf27534ec29431e24efa8b
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-04-26 14:42:18 +0900

    Update

--- experiments/main.py
+++ experiments/main.py
@@ -20,8 +20,8 @@ today = datetime.datetime.now().strftime('%Y.%m.%d')
 
 size = 256
 num_epochs = 10
-train_loader = get_loader(data='REDS', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
-test_loader = get_loader(data='REDS', mode='test', height=256, width=256, scale_factor=4)
+train_loader = get_loader(data='DIV2K', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
+test_loader = get_loader(data='DIV2K', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_REDS_ab_fea', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
 
 
