d56743145481fd4fbb4e967e129169c07abbd3b6
Hyungmin Roh <raingold1347@snu.ac.kr>
2021-04-26 17:11:50 +0900

    Update

--- experiments/main.py
+++ experiments/main.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import torch
 
 import datetime
-
+"""
 from models.hmnet_heavy_ablation_fea import hmnet
 from utils.data_loader import get_loader
 import trainer_0426 as trainer
@@ -64,7 +64,7 @@ num_epochs = 50
 train_loader = get_loader(data='DIV2K', mode='train', batch_size=batch_size, height=size, width=size, scale_factor=4, augment=True)
 test_loader = get_loader(data='DIV2K', mode='test', height=256, width=256, scale_factor=4)
 trainer.train(model, train_loader, test_loader, mode=f'HMNET_DIV2K_ab_edge', epoch_start=0, num_epochs=num_epochs, save_model_every=1, test_model_every=1, today=today, refresh=False)
-
+"""
 
 from models.hmnet_heavy import hmnet
 from utils.data_loader import get_loader
